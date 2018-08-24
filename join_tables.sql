f61555bcc314d13739f87ba395b9f4e18ed1c2b0
Gguidini <giovanni.guidini@gmail.com>
2018-08-24 17:31:39 -0300

    Script to move records to final DB

--- join_tables.sql
+++ join_tables.sql
@@ -17,3 +17,35 @@ CREATE VIEW blast_uniprot_go AS SELECT DISTINCT blast_uniprot.FullAccession, uni
 -- Table final distinct contains relationship between transcripts and their GOs
 CREATE VIEW final AS SELECT DISTINCT * FROM transcript_orf_blast INNER JOIN blast_uniprot_go ON transcript_orf_blast.FullAccession=blast_uniprot_go.FullAccession;
 
+SELECT DISTINCT Transcript.sequence, ORF.peptide, BlastDbase.FullAccession, BlastDbase.GINumber, BlastDbase.PercentIdentity, BlastDbase.Evalue, UniprotIndex.Accession, UniprotIndex.AttributeType, go.id, go.name, go.namespace, go.def, TaxonomyIndex.NCBITaxonomyAccession, TaxonomyIndex.TaxonomyValue, PFAMreference.pfam_accession, PFAMreference.pfam_domainname, PFAMreference.pfam_domaindescription
+FROM Transcript 
+INNER JOIN ORF ON ORF.transcript_id=Transcript.transcript_id
+INNER JOIN BlastDbase ON (BlastDbase.TrinityID=ORF.orf_id OR BlastDbase.TrinityID=ORF.transcript_id)
+INNER JOIN UniprotIndex ON  BlastDbase.FullAccession=UniprotIndex.Accession
+INNER JOIN go ON go.id=UniprotIndex.LinkID
+LEFT JOIN TaxonomyIndex ON TaxonomyIndex.NCBITaxonomyAccession=UniprotIndex.LinkId
+INNER JOIN pfam2go ON go.id=pfam2go.go_id 
+LEFT JOIN PFAMreference ON PFAMreference.pfam_accession=pfam2go.pfam_acc;
+
+-- clean slate
+DROP VIEW transcript_orf;
+DROP VIEW orf_blast;
+DROP VIEW blast_uniprot;
+DROP VIEW uniprot_go;
+DROP VIEW transcript_orf_blast;
+DROP VIEW blast_uniprot_go;
+DROP VIEW final;
+
+-- core data
+CREATE VIEW core_data AS SELECT DISTINCT Transcript.sequence, ORF.peptide, BlastDbase.FullAccession, BlastDbase.GINumber, BlastDbase.PercentIdentity, BlastDbase.Evalue, UniprotIndex.Accession, UniprotIndex.AttributeType, go.id, go.name, go.namespace, go.def, UniprotIndex.LinkID, HMMERDbase.HMMERDomain, HMMERDbase.HMMERTDomainDescription, HMMERDbase.FullSeqScore, PFAMreference.pfam_accession, PFAMreference.pfam_domainname, PFAMreference.pfam_domaindescription, TaxonomyIndex.NCBITaxonomyAccession, TaxonomyIndex.TaxonomyValue, eggNOGIndex.eggNOGIndexTerm, eggNOGIndex.eggNOGDescriptionValue
+FROM Transcript 
+INNER JOIN ORF ON ORF.transcript_id=Transcript.transcript_id
+INNER JOIN BlastDbase ON (BlastDbase.TrinityID=ORF.orf_id OR BlastDbase.TrinityID=ORF.transcript_id)
+INNER JOIN UniprotIndex ON  BlastDbase.FullAccession=UniprotIndex.Accession
+INNER JOIN go ON go.id=UniprotIndex.LinkID
+INNER JOIN HMMERDbase ON (HMMERDbase.QueryProtID=ORF.orf_id OR HMMERDbase.QueryProtID=ORF.transcript_id)
+INNER JOIN PFAMreference ON HMMERDbase.pfam_id=PFAMreference.pfam_accession
+LEFT JOIN TaxonomyIndex ON UniprotIndex.LinkId=TaxonomyIndex.NCBITaxonomyAccession
+LEFT JOIN eggNOGIndex ON eggNOGIndex.eggNOGIndexTerm=UniprotIndex.LinkId;
+
+
---
+++ protein_name_getter.py
@@ -0,0 +1,168 @@
+"""
+Script to move protein information from SQLite - created in the annotation process
+to MongoDB - used in the website with Django.
+
+Also unifies SQLite databases, extracts hit counts and retrieves protein names using BioPython.
+
+WARNING: The script takes a lot of time to run depending on Entry size. TIme trackers over major 
+time consuming functions
+"""
+
+# Retrieve Protein Names
+from Bio import ExPASy
+from Bio import SwissProt
+from Bio import SeqIO
+# Connect to MongoDB
+from pymongo import MongoClient
+# Connect to SQLite
+import sqlite3
+from sqlite3 import Error
+# Easily manage SQL Tables in transition
+import pandas as pd
+# Time tracking
+import time
+def getNameAndKeys(gene):
+	""" 
+	Gets name and keywords for Protein given gene.
+	By Waldeyr Mendes, PhD
+	"""
+	try:
+		with ExPASy.get_sprot_raw(gene) as handle:
+			seq_record = SeqIO.read(handle, "swiss")
+		fullName = seq_record.description[14:].split(";")[0]
+		keys = seq_record.annotations["keywords"]
+		return (fullName, keys)
+	except :
+		print("WARNING: %s not found" % gene)
+		return (None, None)
+
+def connect(filename):
+    """ Connects to SQLite DB specified in filename."""
+    try :
+        conn = sqlite3.connect(filename)
+    except Error as e:
+        print(e)
+        exit(1)
+    return conn
+
+def connectInovatoxin():
+	""" Connection to MongoDB """
+	client = MongoClient()
+	db = client['inovatoxin']
+	return db['Proteins_protein']
+
+def uploadDocs(db, df, data):
+	""" 
+	Uploads docs to MongoDB in correct Collection.
+	Docs are generated according to Django Model.
+	"""
+	print("Inserindo Documentos...")
+	total = df.index
+	columns = df.columns
+	fields = ["transcript_sequence", 'orf_peptide', 'Blast_fullAccession', 'Blast_GINumber', 'Blast_percentIdentity', 'Blast_Evalue', 'Uniprot_accession', 'Uniprot_attribute_type', 'GO_id', 'GO_name', 'GO_namespace', 'GO_def', 'HMMER_domain', 'HMMER_domainDescription', 'HMMER_fullSeqEvalue', 'Pfam_accession', 'Pfam_domainName', 'Pfam_domainDescription', 'NCBI_taxonomyAccession', 'NCBI_taxonomyValue', 'EggNOG_indexTerm', 'EggNOG_descriptionValue', 'has_scorpion']
+	for index, row in df.iterrows():
+		print("[{}/{}] - {}\r".format(index, total, row['id']), end="")
+		doc = {}
+		for idx, col in enumerate(columns):
+			doc[fields[idx]] = row[col]
+		t = data[row['FullAccession']]
+		doc['name'] = t[0]
+		doc['keywords'] = t[1]
+		doc['has_wasp'] = 0
+		doc['has_spider'] = 0
+		doc['id'] = index
+		# insert doc in DB
+		db.insert_one(doc)
+	print("Done!")
+
+
+def getTable(conn):
+	""" 
+	Gets the necessary information from SQLite Databases into a pandas DataFrame
+	so it can be manipulated.
+	"""
+	# insecure, but will be used locall, and only once.
+	query = """ 
+	SELECT DISTINCT Transcript.sequence, ORF.peptide, BlastDbase.FullAccession, BlastDbase.GINumber, BlastDbase.PercentIdentity, BlastDbase.Evalue, UniprotIndex.Accession, UniprotIndex.AttributeType, go.id, go.name, go.namespace, go.def, UniprotIndex.LinkID, HMMERDbase.HMMERDomain, HMMERDbase.HMMERTDomainDescription, HMMERDbase.FullSeqScore, PFAMreference.pfam_accession, PFAMreference.pfam_domainname, PFAMreference.pfam_domaindescription, TaxonomyIndex.NCBITaxonomyAccession, TaxonomyIndex.TaxonomyValue, eggNOGIndex.eggNOGIndexTerm, eggNOGIndex.eggNOGDescriptionValue
+	FROM Transcript 
+	INNER JOIN ORF ON ORF.transcript_id=Transcript.transcript_id
+	INNER JOIN BlastDbase ON (BlastDbase.TrinityID=ORF.orf_id OR BlastDbase.TrinityID=ORF.transcript_id)
+	INNER JOIN UniprotIndex ON  BlastDbase.FullAccession=UniprotIndex.Accession
+	INNER JOIN go ON go.id=UniprotIndex.LinkID
+	INNER JOIN HMMERDbase ON (HMMERDbase.QueryProtID=ORF.orf_id OR HMMERDbase.QueryProtID=ORF.transcript_id)
+	INNER JOIN PFAMreference ON HMMERDbase.pfam_id=PFAMreference.pfam_accession
+	LEFT JOIN TaxonomyIndex ON UniprotIndex.LinkId=TaxonomyIndex.NCBITaxonomyAccession
+	LEFT JOIN eggNOGIndex ON eggNOGIndex.eggNOGIndexTerm=UniprotIndex.LinkId
+	"""
+	df = pd.read_sql_query( query, conn)
+	return df
+
+def makeCount(df):
+	""" Returns hitcount for different Proteins' Ids"""
+	return df['LinkId'].value_counts()
+
+
+def getNames(accessions):
+	""" Handles the process of retrieving names and keywords usign function getNameAndKey."""
+	accessions = accessions.unique()
+	print("Getting names...")
+	total = accessions.size
+	done = 0
+	data = {}
+	for acc in accessions:
+		print("[{}/{}] - {}...\r".format(done, total, acc), end="")
+		(f, k) = getNameAndKeys(acc)
+		if k != None:
+			k = ", ".join(k)
+		#print(f, k)
+		data[acc] = (f, k)
+		done += 1
+	print("[{}/{}] - Done!".format(done, total))
+	return data
+
+def getCountsTotal(counts):
+	""" Adds hitcounts of same species """
+	return counts[0].add(counts[1], fill_value=0).add(counts[2], fill_value=0).astype(int)
+
+def main():
+	t_inicial = time.time()
+	prefix = '/home/gguidini/Downloads/SQLite/Trinotate_'
+	files = [ prefix + name for name in ['scorpio_1.sqlite', 'scorpio_2.sqlite', 'scorpio_3.sqlite'] ]#'wasp_1.sqlite', 'wasp_2.sqlite', 'wasp_3.sqlite', 'spider.sqlite']]
+	df = pd.DataFrame()
+	counts = []
+
+	done = 0
+	print("Recuperando dados...")
+	for f in files:
+		print("[{}/3] - {}\r".format(done, f), end="")
+		conn = connect(f)
+		x = getTable(conn)
+		counts.append(makeCount(x))
+		df = pd.concat([df,x]).drop_duplicates().reset_index(drop=True)
+		done += 1
+	print("Tempo para recuperar informação do SQLite e juntar tabelas: {}".format(time.time() - t_inicial))
+
+	# Adiciona contagem
+	t_add = time.time()
+	print("Analisando contagem de Ids")
+	scorpio = getCountsTotal(counts)
+	df['has_scorpion'] = [scorpio[df.iloc[idx]['LinkId']] for idx in df.index]
+	df = df.drop(columns=['LinkId'])
+	print("Tempo de calculo de contagems (Ids): {}".format(time.time() - t_add))
+	t_names = time.time()
+	data = getNames(df['FullAccession'])
+	print("Tempo de recuperar nomes: {}".format(time.time() - t_names))
+
+	db = connectInovatoxin()
+	t_upload = time.time()
+	uploadDocs(db, df, data)
+	print("Tempo de fazer upload: {}".format(time.time() - t_upload))
+
+	print("Tempo total: {}".format(time.time() - t_inicial))
+
+	#wasp = counts[3] + counts[4] + counts[5]
+	#df['has_wasp'] = wasp
+	#df['has_spider'] = counts[6]
+
+if __name__ == '__main__':
+	main()
