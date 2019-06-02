d42cb8ef1eab297405884166ad96a8fe921e9257
Gguidini <giovanni.guidini@gmail.com>
2019-06-02 01:09:32 -0300

    Updating Database. Creating docker for Trinotate

--- make_database/join_tables.sql
+++ make_database/join_tables.sql
@@ -2,50 +2,39 @@
 -- Used to verify interest GOs appearance in the data set 
 -- Used in SQLite
 
-CREATE VIEW transcript_orf AS SELECT Transcript.transcript_id, ORF.orf_id FROM Transcript INNER JOIN ORF ON Transcript.transcript_id=ORF.transcript_id;
-
-CREATE VIEW orf_blast AS SELECT ORF.orf_id, BlastDbase.FullAccession FROM ORF INNER JOIN BlastDbase ON ORF.orf_id=BlastDbase.TrinityID;
-
-CREATE VIEW blast_uniprot AS SELECT BlastDbase.FullAccession, UniprotIndex.LinkID FROM BlastDbase INNER JOIN UniprotIndex ON BlastDbase.FullAccession=UniprotIndex.Accession;
-
-CREATE VIEW uniprot_go AS SELECT go.id FROM UniprotIndex INNER JOIN go ON go.id=UniprotIndex.LinkID;
-
-CREATE VIEW transcript_orf_blast AS SELECT transcript_orf.transcript_id, transcript_orf.orf_id, orf_blast.FullAccession FROM transcript_orf INNER JOIN orf_blast ON transcript_orf.orf_id=orf_blast.orf_id;
-
-CREATE VIEW blast_uniprot_go AS SELECT DISTINCT blast_uniprot.FullAccession, uniprot_go.id FROM blast_uniprot INNER JOIN uniprot_go ON blast_uniprot.LinkID=uniprot_go.id;
-
--- Table final distinct contains relationship between transcripts and their GOs
-CREATE VIEW final AS SELECT DISTINCT * FROM transcript_orf_blast INNER JOIN blast_uniprot_go ON transcript_orf_blast.FullAccession=blast_uniprot_go.FullAccession;
-
-SELECT DISTINCT Transcript.sequence, ORF.peptide, BlastDbase.FullAccession, BlastDbase.GINumber, BlastDbase.PercentIdentity, BlastDbase.Evalue, UniprotIndex.Accession, UniprotIndex.AttributeType, go.id, go.name, go.namespace, go.def, TaxonomyIndex.NCBITaxonomyAccession, TaxonomyIndex.TaxonomyValue, PFAMreference.pfam_accession, PFAMreference.pfam_domainname, PFAMreference.pfam_domaindescription
-FROM Transcript 
-INNER JOIN ORF ON ORF.transcript_id=Transcript.transcript_id
-INNER JOIN BlastDbase ON (BlastDbase.TrinityID=ORF.orf_id OR BlastDbase.TrinityID=ORF.transcript_id)
-INNER JOIN UniprotIndex ON  BlastDbase.FullAccession=UniprotIndex.Accession
-INNER JOIN go ON go.id=UniprotIndex.LinkID
-LEFT JOIN TaxonomyIndex ON TaxonomyIndex.NCBITaxonomyAccession=UniprotIndex.LinkId
-INNER JOIN pfam2go ON go.id=pfam2go.go_id 
-LEFT JOIN PFAMreference ON PFAMreference.pfam_accession=pfam2go.pfam_acc;
-
--- clean slate
-DROP VIEW transcript_orf;
-DROP VIEW orf_blast;
-DROP VIEW blast_uniprot;
-DROP VIEW uniprot_go;
-DROP VIEW transcript_orf_blast;
-DROP VIEW blast_uniprot_go;
-DROP VIEW final;
-
--- core data
-CREATE VIEW core_data AS SELECT DISTINCT Transcript.sequence, ORF.peptide, BlastDbase.FullAccession, BlastDbase.GINumber, BlastDbase.PercentIdentity, BlastDbase.Evalue, UniprotIndex.Accession, UniprotIndex.AttributeType, go.id, go.name, go.namespace, go.def, UniprotIndex.LinkID, HMMERDbase.HMMERDomain, HMMERDbase.HMMERTDomainDescription, HMMERDbase.FullSeqScore, PFAMreference.pfam_accession, PFAMreference.pfam_domainname, PFAMreference.pfam_domaindescription, TaxonomyIndex.NCBITaxonomyAccession, TaxonomyIndex.TaxonomyValue, eggNOGIndex.eggNOGIndexTerm, eggNOGIndex.eggNOGDescriptionValue
-FROM Transcript 
-INNER JOIN ORF ON ORF.transcript_id=Transcript.transcript_id
-INNER JOIN BlastDbase ON (BlastDbase.TrinityID=ORF.orf_id OR BlastDbase.TrinityID=ORF.transcript_id)
-INNER JOIN UniprotIndex ON  BlastDbase.FullAccession=UniprotIndex.Accession
-INNER JOIN go ON go.id=UniprotIndex.LinkID
-INNER JOIN HMMERDbase ON (HMMERDbase.QueryProtID=ORF.orf_id OR HMMERDbase.QueryProtID=ORF.transcript_id)
-INNER JOIN PFAMreference ON HMMERDbase.pfam_id=PFAMreference.pfam_accession
-LEFT JOIN TaxonomyIndex ON UniprotIndex.LinkId=TaxonomyIndex.NCBITaxonomyAccession
-LEFT JOIN eggNOGIndex ON eggNOGIndex.eggNOGIndexTerm=UniprotIndex.LinkId;
+SELECT 
+    Transcript.sequence AS Sequence, 
+    ORF.peptide AS Peptide, 
+    Transcript.origin_pair AS `From Pair`,
+    BlastDbase.FullAccession AS `Blast Acession`, 
+    BlastDbase.PercentIdentity AS `Blast Identity`, 
+    BlastDbase.Evalue AS `Blast E-value`, 
+    UniprotIndex.Accession AS `Uniprot Acession`, 
+    UniprotIndex.AttributeType AS `Uniprot Attr Type`, 
+    go.id AS `GO`, 
+    go.name AS `GO Name`, 
+    go.namespace AS `GO Namespace`, 
+    go.def AS `GO Def`, 
+    TaxonomyIndex.NCBITaxonomyAccession AS `Taxonomy Index`, 
+    TaxonomyIndex.TaxonomyValue AS `Taxonomy Value`, 
+    PFAMreference.pfam_accession, 
+    PFAMreference.pfam_domainname, 
+    PFAMreference.pfam_domaindescription
+FROM 
+    Transcript 
+LEFT JOIN ORF ON 
+    ORF.transcript_id=Transcript.transcript_id
+LEFT JOIN BlastDbase ON 
+    (BlastDbase.TrinityID=ORF.orf_id OR BlastDbase.TrinityID=Transcript.transcript_id)
+LEFT JOIN UniprotIndex ON 
+    BlastDbase.FullAccession=UniprotIndex.Accession
+LEFT JOIN go ON 
+    go.id=UniprotIndex.LinkID
+LEFT JOIN TaxonomyIndex ON 
+    TaxonomyIndex.NCBITaxonomyAccession=UniprotIndex.LinkId
+LEFT JOIN pfam2go ON 
+    go.id=pfam2go.go_id 
+LEFT JOIN PFAMreference ON 
+    PFAMreference.pfam_accession=pfam2go.pfam_acc;
 
 
